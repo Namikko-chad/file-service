eced6e4a95ee068da02db0a8f9460114120ce13f
Leonhard Schmidt <bloodheavendevelop@gmail.com>
2023-09-18 14:30:59 +0500

    feat(dto): added file info interface

--- src/app/files/dto/file-info.dto.ts
+++ src/app/files/dto/file-info.dto.ts
@@ -1,7 +1,18 @@
 import { ApiProperty, } from '@nestjs/swagger';
 import { IsBoolean, IsPositive, IsString, IsUUID, } from 'class-validator';
 
-export class FileInfoDto {
+export interface FileInfo {
+  id: string;
+  name: string;
+  ext: string;
+  mime: string;
+  size: number;
+  public: boolean;
+  userId: string;
+  hash: string;
+}
+
+export class FileInfoDto implements FileInfo {
   @ApiProperty({})
   @IsUUID()
     id: string;
